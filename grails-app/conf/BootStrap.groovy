9a74356b2800ccfd50a1d899ed41eb21ff231c74
Simeon Burns <s-burns@swau.edu>
2013-03-19 00:30:21 -0500

    Security Finished

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -7,8 +7,12 @@ class BootStrap {
       if (!roleAdmin){
            roleAdmin = new Role(authority: 'ROLE_ADMIN').save()
       }
-        User user = User.executeQuery("select u from User u, UserRole ur where u.id = ur.user.id and ur.role.authority = 'ROLE_ADMIN'")
+        List<User> users = User.executeQuery("select u from User u, UserRole ur where u.id = ur.user.id and ur.role.authority = 'ROLE_ADMIN'")
+        if (!users){
 
+        User admin = new User(username:  'admin', password: 'admin', name: 'Admin User').save()
+        UserRole.create(admin, roleAdmin)
+        }
 
     }
     def destroy = {
